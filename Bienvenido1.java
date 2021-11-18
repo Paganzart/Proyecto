e19ba3b26dea4c6eb949c9ccd41c18b8d330bfe4
Alex <redencionzodiaco@hotmail.com>
2021-11-18 14:39:28 -0600

    My first commit

---
+++ Bienvenido1.java
@@ -0,0 +1,11 @@
+// El primer programa en Java
+
+public class Bienvenido1
+{
+   // El metodo main empieza la ejecucion de la aplicacion en Java
+   public static void main( String args[] )
+   {
+       System.out.println( "Bienvenido a la programacion en Java!" );
+  
+   }    // fin del metodo main
+}  // fin de la clase Bienvenido1
